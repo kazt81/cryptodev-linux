dff00ac1e3a45e5fb70f8efad499aa4a2476a504
Kazuki Terada <kazuki.terada@sonymobile.com>
2015-07-02 15:22:44 +0900

    cleanup example files

--- examples/fipsaes.h
+++ examples/fipsaes.h
@@ -4,16 +4,16 @@
 #include <stdint.h>
 
 struct cryptodev_ctx {
-  int cfd;
-  struct session_op sess;
-  uint16_t alignmask;
+	int cfd;
+	struct session_op sess;
+	uint16_t alignmask;
 };
 
 #define	AES_BLOCK_SIZE (16)
 
-int aes_ctx_init(struct cryptodev_ctx* ctx, int cfd, const uint8_t *key, unsigned int key_size);
-void aes_ctx_deinit();
-int aes_encrypt(struct cryptodev_ctx* ctx, const void* iv, const void* plaintext, void* ciphertext, size_t size);
-int aes_decrypt(struct cryptodev_ctx* ctx, const void* iv, const void* ciphertext, void* plaintext, size_t size);
+int fips_aes_ctx_init(struct cryptodev_ctx* ctx, int cfd, const uint8_t *key, unsigned int key_size);
+void fips_aes_ctx_deinit();
+int fips_aes_encrypt(struct cryptodev_ctx* ctx, const void* iv, const void* plaintext, void* ciphertext, size_t size);
+int fips_aes_decrypt(struct cryptodev_ctx* ctx, const void* iv, const void* ciphertext, void* plaintext, size_t size);
 
 #endif
